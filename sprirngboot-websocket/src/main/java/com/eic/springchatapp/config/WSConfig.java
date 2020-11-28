5dd97ba446bb3996ab0e816263d1e12ebf327d08
Eic Dafusen <bahadirhamzagul@gmail.com>
2020-11-28 18:41:02 +0300

    Added interceptor to WebSocket for authentication

--- sprirngboot-websocket/src/main/java/com/eic/springchatapp/config/WSConfig.java
+++ sprirngboot-websocket/src/main/java/com/eic/springchatapp/config/WSConfig.java
@@ -1,18 +1,26 @@
 package com.eic.springchatapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.eic.springchatapp.interceptor.AuthChannelInterceptor;
+import com.eic.springchatapp.security.UserAuthenticationProvider;
+
+
+
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WSConfig implements WebSocketMessageBrokerConfigurer {
 
-	
+	@Autowired
+	UserAuthenticationProvider userAuthenticationProvider;
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -27,9 +35,14 @@ public class WSConfig implements WebSocketMessageBrokerConfigurer {
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		//for getting messages
 		registry.enableSimpleBroker("/topic");
-		registry.setApplicationDestinationPrefixes("/app");
 	}
 	
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+	    registration.setInterceptors(new AuthChannelInterceptor(userAuthenticationProvider));
+	}
 
 
 }
